62fe63cc934e88cae3fa944c0076b9b1eeef0ef6
Andres Clavijo <andrclam@outlook.com>
2018-12-08 19:53:08 -0500

    AddingFile Writer class

---
+++ AppUtils/FileWriter.cs
@@ -0,0 +1,11 @@
+namespace servicioCliente.AppUtils
+{
+    public class FileWriter
+    {
+        public bool WriteOnFile(string fileName,string message){
+            bool operationSucces = false;
+            
+            return operationSucces;
+        }
+    }
+}
